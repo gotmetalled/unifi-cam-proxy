ccb8658442da85dc6c56f8498d4bd3849b595c66
Simon Lacher <lach.sim@gmail.com>
2022-01-05 14:46:40 +0100

    changed s3v and doorbird

--- unifi/cams/__init__.py
+++ unifi/cams/__init__.py
@@ -3,5 +3,7 @@ from unifi.cams.frigate import FrigateCam
 from unifi.cams.hikvision import HikvisionCam
 from unifi.cams.reolink_nvr import ReolinkNVRCam
 from unifi.cams.rtsp import RTSPCam
+from unifi.cams.doorbird import DoorbirdCam
+from unifi.cams.s3v import S3VCam
 
-__all__ = ["FrigateCam", "HikvisionCam", "DahuaCam", "RTSPCam", "ReolinkNVRCam"]
+__all__ = ["FrigateCam", "HikvisionCam", "DahuaCam", "RTSPCam", "ReolinkNVRCam", "DoorbirdCam", "S3VCam"]
